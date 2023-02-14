9c2b04df0076774a0a26d43296521cd5f652a1b2
Wing <winghangsong@gmail.com>
2023-02-14 08:25:17 -0500

    Adding styles to REST-Rant

--- index.js
+++ index.js
@@ -1,11 +1,15 @@
+// Modules and Globals
 require('dotenv').config()
 const express = require('express')
-
 const app = express()
 
+//Express
+app.set('views', __dirname + '/views')
 app.set('view engine', 'jsx')
 app.engine('jsx', require('express-react-views').createEngine())
+app.use(express.static('public'))
 
+// Controllers & Routes
 app.use('/places', require('./controllers/places'))
 
 app.get('/', function (req, res) {
@@ -16,4 +20,5 @@ app.get('*', function (req, res) {
     res.render('error404')
 })
 
+//Listening for Connections
 app.listen(process.env.PORT)
