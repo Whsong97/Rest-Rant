c73538d3711259c0f14b89c529dfc1c04259d5c4
Wing <winghangsong@gmail.com>
2023-02-16 09:35:54 -0500

    Delete functionality impletmented

--- controllers/places.js
+++ controllers/places.js
@@ -1,7 +1,7 @@
 const router = require('express').Router()
 const places = require('../models/places')
 
-router.post('/', (req, res) => {
+router.post('/', function(req, res){
     if (!req.body.pic) {
         // Default image if one is not provided
         req.body.pic = 'https://j4y2v5w8.stackpathcdn.com/wp-content/uploads/2017/03/Generic-vs.-Brand-Name.jpg'
@@ -25,7 +25,7 @@ router.get('/:id', function(req, res){
         res.render('error404')
     }
     else {
-        res.render('places/show', { place: places[id] })
+        res.render('places/show', { place: places[id], id })
     }
 })
    
@@ -33,8 +33,21 @@ router.get('/new', function(req, res){
     res.render('places/new')
 })
 
-    router.get('/places', function(req, res) {
+router.get('/', function(req, res){
     res.render('places/index', { places })
 })
 
+router.delete('/:id', function(req, res){
+    let id = Number(req.params.id)
+    if (isNaN(id)) {
+        res.render('error404')
+    }
+    else if (!places[id]) {
+        res.render('error404')
+    }
+    else {
+        places.splice(id, 1)
+        res.redirect('/places')
+    }
+})
 module.exports = router
